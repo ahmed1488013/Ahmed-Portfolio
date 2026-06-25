70b5a6ac6e8bc935916b086ce1dd3d6d7a2b715c
Ahmed Hamada <ah625634@gmail.com>
2026-06-25 18:10:23 +0300

    Upload My Portflio

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,114 @@
+import project1_img from '../assets/hospital.jpeg'
+import project2_img from '../assets/youtube.png'
+import project3_img from '../assets/project_3.svg'
+import project4_img from '../assets/netflix.jpeg'
+import project5_img from '../assets/sass.png'
+import project6_img from '../assets/movies.png'
+import project7_img from '../assets/store.png'
+import project8_img from '../assets/time.png'
+import project9_img from '../assets/portoflio.png'
+import project10_img from '../assets/dashbord-github.png'
+import project11_img from '../assets/forge.png'
+import project12_img from '../assets/games.png'
+import project13_img from '../assets/finance.png'
+import project14_img from '../assets/blog.png'
+import project15_img from '../assets/Image_Editor.png'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"Web design",
+        w_img:project1_img,
+        link:"https://prescriptor-e874c.web.app/",
+    },
+    {
+        w_no:2,
+        w_name:"Web design",
+        w_img:project2_img,
+        link:"https://lone-1d2ea.web.app/"
+    },
+    {
+        w_no:3,
+        w_name:"Web design",
+        w_img:project3_img,
+        link:"https://dashbord-d65ed.web.app/"
+    },
+    {
+        w_no:4,
+        w_name:"Web design",
+        w_img:project6_img,
+        link:"https://benevolent-crostata-08f238.netlify.app/"
+        
+    },
+    {
+        w_no:5,
+        w_name:"Web design",
+        w_img:project11_img,
+        link:"https://forge-sandy-nine.vercel.app/"
+    },
+    {
+        w_no:6,
+        w_name:"Web design",
+        w_img:project12_img,
+        link:"https://amazing-piroshki-0a2483.netlify.app/"
+    },
+    {
+        w_no:7,
+        w_name:"Web design",
+        w_img:project13_img,
+        link:"https://clinquant-monstera-ef5a3a.netlify.app/"
+    },
+    {
+        w_no:8,
+        w_name:"Web design",
+        w_img:project14_img,
+        link:"https://blog-app-fullstack-progect.netlify.app/"
+    },
+    {
+        w_no:9,
+        w_name:"Web design",
+        w_img:project15_img,
+        link:"https://ahmedimg.netlify.app/"
+    },
+    {
+        w_no:10,
+        w_name:"Web design",
+        w_img:project4_img,
+        link:" https://netflix-e86c4.web.app/"
+    },
+    {
+        w_no:11,
+        w_name:"Web design",
+        w_img:project7_img,
+        link:"https://steady-bonbon-63cb37.netlify.app/"
+    },
+    {
+        w_no:12,
+        w_name:"Web design",
+        w_img:project8_img,
+        link:"https://resurant-ahmed.netlify.app/"
+    },
+    {
+        w_no:13,
+        w_name:"Web design",
+        w_img:project9_img,
+        link:"https://stupendous-klepon-57e278.netlify.app/"
+    },
+    {
+        w_no:14,
+        w_name:"Web design",
+        w_img:project10_img,
+        link:"https://frolicking-platypus-7877b0.netlify.app/"
+    },
+    {
+        w_no:15,
+        w_name:"Web design",
+        w_img:project5_img,
+        link:"https://ahmed1488013.github.io/Sass-Design/"
+
+    },
+    
+
+]
+ 
+export default mywork_data;
