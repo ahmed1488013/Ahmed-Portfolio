2ca8d9755a6ea2ba8b4892397c7e324ae87875c9
Ahmed Hamada <ah625634@gmail.com>
2026-06-26 21:06:29 +0300

    Update project

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,18 +1,16 @@
 import project1_img from '../assets/hospital.jpeg'
 import project2_img from '../assets/youtube.png'
 import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/netflix.jpeg'
-import project5_img from '../assets/sass.png'
-import project6_img from '../assets/movies.png'
-import project7_img from '../assets/store.png'
-import project8_img from '../assets/time.png'
-import project9_img from '../assets/portoflio.png'
-import project10_img from '../assets/dashbord-github.png'
-import project11_img from '../assets/forge.png'
-import project12_img from '../assets/games.png'
-import project13_img from '../assets/finance.png'
-import project14_img from '../assets/blog.png'
-import project15_img from '../assets/Image_Editor.png'
+import project4_img from '../assets/movies.png'
+import project5_img from '../assets/forge.png'
+import project6_img from '../assets/games.png'
+import project7_img from '../assets/finance.png'
+import project8_img from '../assets/Image_Editor.png'
+import project9_img from '../assets/netflix.jpeg'
+import project10_img from '../assets/store.png'
+import project11_img from '../assets/portoflio.png'
+import project12_img from '../assets/cv.png'
+import project13_img from '../assets/sass.png'
 
 const mywork_data = [
     {
@@ -36,62 +34,62 @@ const mywork_data = [
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project6_img,
+        w_img:project4_img,
         link:"https://egypest.vercel.app/"
         
     },
     {
         w_no:5,
         w_name:"Web design",
-        w_img:project11_img,
+        w_img:project5_img,
         link:"https://forge-sandy-nine.vercel.app/"
     },
     {
         w_no:6,
         w_name:"Web design",
-        w_img:project12_img,
+        w_img:project6_img,
         link:"https://games-rho-gold.vercel.app/"
     },
     {
         w_no:7,
         w_name:"Web design",
-        w_img:project13_img,
+        w_img:project7_img,
         link:"https://ahmed-finance.vercel.app/"
     },
     {
         w_no:8,
         w_name:"Web design",
-        w_img:project14_img,
-        link:"https://blog-app-mu-hazel.vercel.app/"
+        w_img:project8_img,
+        link:"https://image-editor-five-rosy.vercel.app/"
     },
     {
         w_no:9,
         w_name:"Web design",
-        w_img:project15_img,
-        link:"https://image-editor-five-rosy.vercel.app/"
+        w_img:project9_img,
+        link:"https://netflix-ten-silk.vercel.app/"
     },
     {
         w_no:10,
         w_name:"Web design",
-        w_img:project4_img,
-        link:"https://netflix-ten-silk.vercel.app/"
+        w_img:project10_img,
+        link:"https://store-one-xi-70.vercel.app/"
     },
     {
         w_no:11,
         w_name:"Web design",
-        w_img:project7_img,
-        link:"https://store-one-xi-70.vercel.app/"
+        w_img:project11_img,
+        link:"https://port-portfolio-eta.vercel.app/"
+    },
+    {
+        w_no:12,
+        w_name:"Web design",
+        w_img:project12_img,
+        link:"https://a7med-cv.vercel.app/"
     },
     {
         w_no:13,
         w_name:"Web design",
-        w_img:project9_img,
-        link:"https://port-portfolio-eta.vercel.app/"
-    },
-    {
-        w_no:15,
-        w_name:"Web design",
-        w_img:project5_img,
+        w_img:project13_img,
         link:"https://ahmed1488013.github.io/Sass-Design/"
 
     },
